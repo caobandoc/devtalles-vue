3a97159496398df20aa1c723c0b70546be067ec7
caobandoc <obandocarlosandres@gmail.com>
2024-09-10 22:16:27 -0500

    template in html

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -1,13 +1,6 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
-    template: `
-    <h1>Hola mundo</h1>
-    <p>Desde app.js</p>
-    <h1>Reactividad:</h1>
-    <p>{{message}}</p>
-    <p>{{author}}</p>
-  `,
     setup() {
         const message = ref("I'm batman");
         const author = ref("Bruce Wayne");
